a9dbd0c26965720957137ec9f45376710bd42569
mytomga1 <mr.mytomga1@gmail.com>
2022-06-29 23:23:01 +0700

    Upload Post[index + 1/2 create]

---
+++ MVC_BaoOnline/BaoMoiOnline/BaoMoiOnline/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BaoMoiOnline.Models;
+using BaoOnline.Helpers;
+using PagedList.Core;
+
+namespace BaoMoiOnline.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostsController : Controller
+    {
+        private readonly BaoOnlineContext _context;
+
+        public PostsController(BaoOnlineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Posts
+        //public async Task<IActionResult> Index()
+        //{
+        //    var baoOnlineContext = _context.Posts.Include(p => p.Account).Include(p => p.Cat);
+        //    return View(await baoOnlineContext.ToListAsync());
+        //}
+
+        public IActionResult Index(int? page)
+        {
+
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;// nếu page bằng null thì gắng giá trị bằng 1, còn ko thì giá trị = value
+            var pageSize = Utilities.PAGE_SIZE;// PAGE_SIZE ở thư mục Utilities dc gán giá trị sẳn là 20
+            var lsPosts = _context.Posts
+                .Include(p => p.Account).Include(p => p.Cat)
+                .OrderByDescending(x => x.CatId); // sắp xếp theo CatId
+
+            // gọi sử dụng thư viện PagedList
+            PagedList<Post> models = new PagedList<Post>(lsPosts, pageNumber, pageSize);
+
+            //ViewBag.CurrentPage = pageNumber;
+            return View(models);
+        }
+
+        // GET: Admin/Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Account)
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Create
+        public IActionResult Create()
+        {
+            // ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId");
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName");
+            return View();
+        }
+
+        // POST: Admin/Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostId,Title,ShortContents,Contents,Img,Alias,CreateDate,Author,AccountId,Tag,CatId,IsHost,IsNewfeed")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", post.AccountId);
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", post.CatId);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", post.AccountId);
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", post.CatId);
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostId,Title,ShortContents,Contents,Img,Alias,CreateDate,Author,AccountId,Tag,CatId,IsHost,IsNewfeed")] Post post)
+        {
+            if (id != post.PostId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.PostId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", post.AccountId);
+            ViewData["DanhMuc"] = new SelectList(_context.Categories, "CatId", "CatName", post.CatId);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Account)
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.PostId == id);
+        }
+    }
+}
